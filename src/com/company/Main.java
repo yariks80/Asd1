f9ba8c4ed9cf7e4e33faca7e4c39e5edb303e9b1
Iaroslav <yariks@i.ua>
2017-06-04 09:56:27 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         String [] s = scanner.nextLine().split("\\s");
-        System.out.println(Arrays.toString(s));
+        Sysvvvvvvvvvvvvvvtem.out.println(Arrays.toString(s));
+        
     }
 }
